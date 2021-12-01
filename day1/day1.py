823e9b41795b622b640dc8d846aff3a9ea5094c2
Lovis Heindrich <33863801+lovis-heindrich@users.noreply.github.com>
2021-12-01 17:01:23 +0100

    day1

---
+++ day1/day1.py
@@ -0,0 +1,13 @@
+def window_sum(numbers, length):
+    counter = 0
+    for i in range(len(numbers)-length):
+        if sum(numbers[i:i+length]) < sum(numbers[i+1:i+length+1]):
+            counter += 1
+    return counter
+
+if __name__ == "__main__":
+    with open('./measurements.txt') as f:
+        lines = [int(x) for x in f.read().splitlines()]
+    
+    print("Part 1:", window_sum(lines, 1)) 
+    print("Part 2:", window_sum(lines, 3))
