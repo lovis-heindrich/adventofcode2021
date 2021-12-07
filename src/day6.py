71ac4bde90bde0036fa42b22143e737be4f778e3
Lovis Heindrich <33863801+lovis-heindrich@users.noreply.github.com>
2021-12-07 15:47:24 +0100

    more elegant solution

--- src/day6.py
+++ src/day6.py
@@ -9,8 +9,17 @@ def fast_day(population, n):
         counts.append(new_spawns)
     return sum(counts)
 
+def efficient_day(population, n):
+    max_count = INTERVAL+DELAY
+    counts = [population.count(x) for x in range(max_count)]
+    for i in range(n):
+        zero_index = i % max_count
+        counts[(zero_index + INTERVAL) % max_count] += counts[zero_index]
+    return sum(counts)
+
 if __name__ == "__main__":
     with open("./data/day6.txt") as f:
         population = [int(x) for x in f.read().split(",")]
-    print("Part 1:", fast_day(population, 80))
-    print("Part 2:", fast_day(population, 256))
+
+    print("Part 1:", efficient_day(population, 80))
+    print("Part 2:", efficient_day(population, 256))
